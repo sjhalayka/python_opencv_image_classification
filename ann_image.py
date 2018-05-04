1f94e8ccd93cb10148b23042b5d5aef471b1fe87
Shawn Halayka <sjhalayka@gmail.com>
2018-05-04 15:14:43 -0600

    Add files via upload

--- ann_image.py
+++ ann_image.py
@@ -94,7 +94,9 @@ for line in training_file:
 
 # It might be a good idea to pseudorandomly shuffle the filenames/classifications
 # This uses an O(n^2) algorithm to shuffle -- using the length squared
-training_filenames, training_classifications = shuffle(training_filenames, training_classifications, len(training_filenames)*len(training_filenames))
+training_filenames, training_classifications = shuffle(training_filenames,
+                                                       training_classifications,
+                                                       len(training_filenames)*len(training_filenames))
 
 # Get the maximum classification number
 max_class = 0
