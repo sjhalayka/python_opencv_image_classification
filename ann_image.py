0b55fe166b6a99a3c69685ca8e1188171d9b9673
Shawn Halayka <sjhalayka@gmail.com>
2018-05-06 17:21:22 -0600

    Add files via upload

--- ann_image.py
+++ ann_image.py
@@ -151,7 +151,7 @@ img_td = cv2.ml.TrainData_create(img_input_array, cv2.ml.ROW_SAMPLE, img_output_
 ann.train(img_td, cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
 
 # For each further training iteration, update the weights
-for i in range(0, 1):
+for i in range(0, 1000):
     print(i)
 
     # For each file in the training data
